def329520647b0c13b2d120df31440eb1e479b90
satyamdas03 <116027621+satyamdas03@users.noreply.github.com>
2022-12-29 20:45:18 +0530

    Updates

---
+++ precache-manifest.bd7c642df81c7b4c2eedd8c76fcdca58.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "49577822d0639998ee7bcd4edcf78c8c",
+    "url": "/keeper-app/index.html"
+  },
+  {
+    "revision": "ca51de6774748b76c0e0",
+    "url": "/keeper-app/static/js/2.033aed29.chunk.js"
+  },
+  {
+    "revision": "01d7b0816a0673bf89b2",
+    "url": "/keeper-app/static/js/main.9a88ca9b.chunk.js"
+  },
+  {
+    "revision": "7497e34bc9a978950d71",
+    "url": "/keeper-app/static/js/runtime-main.5f33c95a.js"
+  }
+]);
